62c941937d44682e47031f616648fee6443c2672
1990abhay <1990abhay@gmail.com>
2025-07-21 11:22:08 +0530

    Add files via upload

---
+++ API-Gatway/src/test/java/com/mymicroservices/API_Gatway/ApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymicroservices.API_Gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

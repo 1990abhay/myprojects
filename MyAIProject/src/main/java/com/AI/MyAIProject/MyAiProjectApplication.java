53b45a588ea4baa7e92144ff8360fd917306c4ae
1990abhay <1990abhay@gmail.com>
2025-08-04 13:41:42 +0530

    aiproject

---
+++ MyAIProject/src/main/java/com/AI/MyAIProject/MyAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.AI.MyAIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAiProjectApplication.class, args);
+	}
+
+}
---
+++ MyAIProject/src/test/java/com/AI/MyAIProject/MyAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI.MyAIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
